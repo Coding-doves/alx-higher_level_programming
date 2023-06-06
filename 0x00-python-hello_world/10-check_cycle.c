97531535f46b322f4957e15a1eede55f628593c5
Coding-doves <obenedicta4@gmail.com>
2023-06-05 22:23:26 -0700

    py Hello world

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,11 +12,10 @@ int check_cycle(listint_t *list)
 
 	ptr = ptr2 = list;
 
-	while(ptr != NULL && ptr->next != NULL)
+	while (ptr != NULL && ptr->next != NULL)
 	{
 		ptr = ptr->next;
-		
-		if(ptr->next == ptr2)
+		if (ptr == ptr2)
 			return (1);
 	}
 	return (0);
