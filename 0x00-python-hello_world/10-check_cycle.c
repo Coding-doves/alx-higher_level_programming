178ff52d6d0a7fee027062395d6390e7bf0af74a
Coding-doves <obenedicta4@gmail.com>
2023-06-05 17:14:08 -0700

    py Hello world

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,20 +1,24 @@
 #include "lists.h"
 
 /**
- * check_cycle - check if a list has cycle
+ * check_cycle - check if a list has cycular linked list
  * @list: linked list input
  * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 
 int check_cycle(listint_t *list)
 {
-	listint_t *ptr = list;
+	listint_t *ptr, *ptr2;
 
-	while(ptr->next != NULL)
+	ptr = ptr2 = list;
+
+	while(ptr != NULL && ptr2 != NULL && ptr2->next != NULL && ptr->next != NULL)
 	{
-		ptr = ptr->next;
-		if(ptr->next == list)
-			return (0);
+		ptr2 = ptr2->next;
+		ptr = ptr->next->next;
+		
+		if(ptr2->next == ptr->next->next)
+			return (1);
 	}
-	return (-1);
+	return (0);
 }
