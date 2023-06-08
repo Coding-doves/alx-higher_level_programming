7e9ac73b37af6d9588fd63df22c0a773b4e3dfc0
Coding-doves <obenedicta4@gmail.com>
2023-06-08 03:27:05 -0700

    py import modules

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+
+    res = add(a, b)
+    print("{} + {} = {}".format(a, b, res))
+    res = sub(a, b)
+    print("{} - {} = {}".format(a, b, res))
+    res = mul(a, b)
+    print("{} * {} = {}".format(a, b, res))
+    res = div(a, b)
+    print("{} / {} = {}".format(a, b, res))
