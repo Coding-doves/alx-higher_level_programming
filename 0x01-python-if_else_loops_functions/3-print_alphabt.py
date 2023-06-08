62f0ffe24271f8ec20d9b1515e6321a0de091272
Coding-doves <obenedicta4@gmail.com>
2023-06-08 09:48:42 -0700

    py if/else statement

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@
 for alpha in range(97, 123):
     if chr(alpha) == 'q' or chr(alpha) == 'e':
                 continue
-    print({}.format(chr(alpha), end='')
+    print("{}".format(chr(alpha)), end='')
