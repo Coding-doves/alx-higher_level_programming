ecb9c336ad8282b012abb171ed746cf2e7761605
Coding-doves <obenedicta4@gmail.com>
2023-06-10 12:59:17 -0700

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,12 @@
 def print_matrix_integer(matrix=[[]]):
 
     for col_m in matrix:
-        for row_m in col_m:
+        for row_m in col_m[:-1]:
             print("{:d}".format(row_m), end=" ")
-        print()
+        if len(col_m) > 0:
+            print("{:d}".format(col_m[-1]))
+        else:
+            print()
     """
     # 2nd code style. both are perfect
     col_m = matrix
