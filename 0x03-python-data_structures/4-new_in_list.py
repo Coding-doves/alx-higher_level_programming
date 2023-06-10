ca9a5fd6a59f687d326cd97fc14e59efa2ce5e55
Coding-doves <obenedicta4@gmail.com>
2023-06-10 04:17:12 -0700

    py Lists

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,13 +2,9 @@
 
 def new_in_list(my_list, idx, element):
 
-    if idx < 0:
+    if idx < 0 or idx >= len(my_list):
         return(my_list)
-    if idx > len(my_list) - 1:
-        return(my_list)
-
-    new_ls = list(my_list)
-    for i in range(len(new_ls)):
-        if new_ls[i] == idx:
-            new_ls[idx] = element
-    return(new_ls)
+    else:
+        new_ls = list(my_list)
+        new_ls[idx] = element
+        return(new_ls)
