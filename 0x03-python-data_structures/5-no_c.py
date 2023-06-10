cd75a503ca144ca3b4f5361f0e0a0f667b6e231e
Coding-doves <obenedicta4@gmail.com>
2023-06-10 09:40:19 -0700

    rm character

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     new = " "
-    for i in range(len(my_string)):
-        if my_string[i] != 'c' and my_string[i] != 'C':
-            new = new + my_string[i]
-        else:
-            del my_string[i]
+    for s in my_string:
+        if s != 'c' and s != 'C':
+            new = new + s
     return(new)
