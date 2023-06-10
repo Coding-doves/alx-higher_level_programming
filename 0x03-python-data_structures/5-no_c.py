607dd56c4665cf9a0ca1892ed04673a27a53eadd
Coding-doves <obenedicta4@gmail.com>
2023-06-10 11:36:27 -0700

    replaces an element in a list

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
     new = ""
     for s in my_string:
         if s != 'c' and s != 'C':
-            new = new + s
-    return(new)
+            new += s
+    return new
