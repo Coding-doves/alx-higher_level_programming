1ebb399422c0dea52c616e1452054447f119e1f8
Coding-doves <obenedicta4@gmail.com>
2023-06-10 03:28:31 -0700

    py Lists

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if sentence:
-        return(len(sentence), sentence[i])
-    else:
-        return(None, None)
+    for i in range(len(sentence)):
+        if sentence:
+            return(len(sentence), sentence[i])
+        else:
+            return(None, None)
