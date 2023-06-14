e845609fa1f2b8e2fca79cafc67bb225a25c91b1
Coding-doves <obenedicta4@gmail.com>
2023-06-14 14:58:01 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i, lit in enumerate(my_list):
+    s = []
+    for lit in my_list:
         if lit == search:
-            my_list[i] = replace
-    return my_list
+            s.append(replace)
+        else:
+            s.append(lit)
+    return s
