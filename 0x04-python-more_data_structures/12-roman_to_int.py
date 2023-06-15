a08c29ec1418028be79dd589c30582d608b07497
Coding-doves <obenedicta4@gmail.com>
2023-06-14 19:39:08 -0700

    roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    rom_num = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000
+            }
+    add = 0
+    l = len(roman_string)
+
+    for i in range(l):
+        if i < l - 1 and rom_num[roman_string[i]] < rom_num[roman_string[i + 1]]:
+            add -= rom_num[roman_string[i]]
+        else:
+            add += rom_num[roman_string[i]]
+    return add
