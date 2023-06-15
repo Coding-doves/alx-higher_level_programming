fdcd174192cb4449051f92dc10cddc80f60a8239
Ada <106437177+Coding-doves@users.noreply.github.com>
2023-06-14 20:01:47 -0700

    Update 12-roman_to_int.py
    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
-        return 0
     rom_num = {
             'I': 1,
             'V': 5,
