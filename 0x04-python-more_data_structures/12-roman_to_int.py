67c21c698222c9c07ffd4ff9b3e67df1f9626d58
Coding-doves <obenedicta4@gmail.com>
2023-06-14 19:51:37 -0700

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
     rom_num = {
-            'I' : 1,
-            'V' : 5,
-            'X' : 10,
-            'L' : 50,
-            'C' : 100,
-            'D' : 500,
-            'M' : 1000
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
             }
     add = 0
-    l = len(roman_string)
+    length = len(roman_string)
 
-    for i in range(l):
-        if i < l - 1 and rom_num[roman_string[i]] < rom_num[roman_string[i + 1]]:
+    for i in range(length):
+        if i < l - 1 and rom_num[roman_string[i]]"/"
+        < rom_num[roman_string[i + 1]]:
             add -= rom_num[roman_string[i]]
         else:
             add += rom_num[roman_string[i]]
