cad9941237aa45e05eda7a9b9c2345d3b3f9047e
Ada <obenedicta4@gmail.com>
2023-06-26 14:43:52 -0700

    error /exception

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if isinstance(value, int):
-            print("{:d}".format(value))
-            return True
-        else:
-            print("{:d}".format(value))
-            return False
+        int(value)
+        print("{:d}".format(value))
+        return True
     except ValueError:
-        pass
+        return False
