c1881de0a2b3bcb6f653892eda22de34b3417473
Ada <obenedicta4@gmail.com>
2023-06-27 18:23:20 -0700

    oop

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Creating class object Square"""
 
+
 class Square:
     """Creating a square shape"""
     pass
--- 0x06-python-classes/doc.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def has_documentation(obj):
-    return obj.__doc__ is not None
-
-# Example usage:
-print(has_documentation(Square))    # Check if class has documentation
