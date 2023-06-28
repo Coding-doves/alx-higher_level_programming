054993ff0647fcd7b47fb8e68e611518077fe72c
Ada <obenedicta4@gmail.com>
2023-06-27 23:00:19 -0700

    oop

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -43,6 +43,24 @@ class Square:
         else:
             self.__size = value
 
+    @property
+    def position(self):
+        """to retrieve position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """to set position"""
+        if type(value) is tuple and len(value) == 2:
+            for p in value:
+                if p < 0:
+                    raise TypeError
+                    print("position must be a tuple of 2 positive integers")
+            self.__position = value
+        else:
+            raise TypeError
+            print("position must be a tuple of 2 positive integers")
+
     def area(self):
         """Calculates the area of the square
 
@@ -53,18 +71,23 @@ class Square:
         return int(self.size) ** 2
 
     def my_print(self):
-        """that prints in stdout the square with the character #"""
+        """that prints in stdout the square with the character #
+
+        and prints first, the position - this represents the 
+        horizontal/row(position[0])/right and vertical/column(position[1])/down
+        offset of the square's top-left corner. It determines where the square
+        will be printed in relation to the left and top edges of the output.
+
+        """
 
         if self.size > 0:
+            """loop to set position"""
+            for j in range(self.position[1]):
+                if self.position[1] > 0:
+                    print()
+
+            """loop to create square #"""
             for i in range(self.size):
-                for j in range(self.size):
-                    print("{}".format('#'), end="")
-                print()
+                print(" " * self.position[0] + "#" * self.size)
         elif self.size == 0:
             print()
-
-    def position(self):
-        """to retrieve position"""
-
-    def position(self, value):
-        """to set position"""
