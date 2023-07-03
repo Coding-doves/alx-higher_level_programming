1f939258905ade8a89a54213944919d6f9bb1b68
Ada <obenedicta4@gmail.com>
2023-07-02 23:21:43 -0700

    test driven

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,7 +11,7 @@ def add_integer(a, b=98):
         int: result of a + b
     """
     if not a or not b:
-        retur
+        return
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     elif not isinstance(b, int) and not isinstance(b, float):
