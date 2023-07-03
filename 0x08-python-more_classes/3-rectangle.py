2de8c9d27e3b7fd5391720e36213b9e13ffbd2f1
Ada <obenedicta4@gmail.com>
2023-07-03 13:27:27 -0700

    class and object

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -55,7 +55,7 @@ class Rectangle:
 
     '''print area as #'''
     def pr_st(self):
-        if self.__width == 0 or self.__height:
+        if self.__width == 0 or self.__height == 0:
             return ' '
         else:
             return '\n'.join('#' * self.__width for i in range(self.__height))
