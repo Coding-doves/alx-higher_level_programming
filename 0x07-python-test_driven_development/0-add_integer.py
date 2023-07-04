b345251c35e4b00cf57fd8c603598ec52739dc05
Ada <obenedicta4@gmail.com>
2023-07-04 15:18:06 -0700

    test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,17 +10,13 @@ def add_integer(a, b=98):
     """Returns:
         int: result of a + b
     """
-    if a is None or type(a) == str:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif b is None or type(b) == str:
-        raise TypeError("b must be an integer")
-    elif not isinstance(a, int) and not isinstance(a, float):
-        raise TypeError("a must be an integer")
-    elif not isinstance(b, int) and not isinstance(b, float):
+    elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     elif isinstance(a, float):
         a = int(a)
     elif isinstance(b, float):
         b = int(b)
-    elif a and b:
-        return int(a + b)
+
+    return int(a + b)
