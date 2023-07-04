6305e663c45b37b75ae61a0edb104490c8773d68
Ada <obenedicta4@gmail.com>
2023-07-04 12:48:18 -0700

    object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -2,4 +2,3 @@
 def magic_string(list_it=[]):
     list_it.append('BestSchool')
     return ','.join(list_it)
-
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-'''creating a locked class that prevents the user from dynamically creating new instance attributes, except if the new instance attribute is called first_name'''
+'''creating a locked class that prevents the user from dynamically
+creating new instance attributes, except if the new instance
+attribute is called first_name'''
+
+
 class LockedClass(object):
+    '''slots holds the list of instances to be created
+    preventing the any other'''
     __slots__ = ['first_name']
 
     def __init__(self, first_name=''):
