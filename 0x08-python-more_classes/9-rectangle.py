9402ebcff73ac681223a95de4ff2a1e39ccbc45d
Ada <obenedicta4@gmail.com>
2023-07-05 03:43:14 -0700

    object and class

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -97,4 +97,4 @@ class Rectangle:
     '''class method that returns a new Rectangle instance'''
     @classmethod
     def square(cls, size=0):
-        return cls(width = size, height = size)
+        return cls(width=size, height=size)
