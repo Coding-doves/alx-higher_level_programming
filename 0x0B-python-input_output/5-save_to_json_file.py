2241ade6293604f5aa4e1240751e040d25c30065
Ada <obenedicta4@gmail.com>
2023-07-11 13:11:19 -0700

    input annd output

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''import module'''
+
+import json
+'''writes an Object to a text file, using a JSON representation'''
+
+
+def save_to_json_file(my_obj, filename):
+    '''using json.load'''
+    with open(filename, 'w') as f:
+        return json.dump(my_obj, f)
