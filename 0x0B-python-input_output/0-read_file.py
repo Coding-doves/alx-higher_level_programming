e04fe03173cabb0bd4e7ecdfabba42189bbf4310
Ada <obenedicta4@gmail.com>
2023-07-11 08:53:08 -0700

    inpu and output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' function that reads a text file (UTF8) and prints it to stdout'''
+
+
+def read_file(filename=""):
+    ''' the use of with and utf-8'''
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+''' returns an object (Python data structure) represented by a JSON string'''
+
+
+def from_json_string(my_str):
