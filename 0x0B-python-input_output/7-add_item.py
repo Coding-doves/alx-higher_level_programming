6f9c2ef3fda2fc475710c64cef725dee57b0e59b
Ada <106437177+Coding-doves@users.noreply.github.com>
2023-07-12 18:43:17 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,10 +9,11 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 args = sys.argv
 file = "add_item.json"
+list_store = []
 
-with open(file, 'a+', encoding="utf-8") as f:
-    '''using json.load'''
-    list_store = []
-    list_store.extend(args[1:])
-    save_to_json_file(list_store, file)
-    load_from_json_file(file)
+'''using json.load'''
+if (file):
+    list_store_2 = load_from_json_file(file)
+
+list_store.extend(args[1:])
+save_to_json_file(list_store, file)
