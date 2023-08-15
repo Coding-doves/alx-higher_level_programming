d3622df7ad4938740555acb71f26391d36192003
Ada <obenedicta4@gmail.com>
2023-08-15 19:57:58 +0100

    sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,2 @@
 -- creates a table called first_table
-USE mysql;
-CREATE  TABLE IF NOT EXISTS first_table (idINT, name VARCHAR(256));
+CREATE  TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
