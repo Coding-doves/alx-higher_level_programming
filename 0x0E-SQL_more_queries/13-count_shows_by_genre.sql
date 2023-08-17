ec38c97f13c99976e7c28331e8688ab8f82b9749
Ada <obenedicta4@gmail.com>
2023-08-17 23:03:22 +0100

    sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,8 @@
 -- list all shows contained in the hbtn_0d_tvshows database
-SELECT tv_shows.genres.genre_id As genre, 
-    COUNT(tv_shows_genres.shows_id) AS number_of_shows
-FROM tv_shows_genres
-GROUP BY tv_shows_genres.genre_id
+SELECT tv_genres.name As genre, 
+    COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name, tv_genres.id
+HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
