dc4906eca0ab4777ebbdc7635376caee33a2291e
Ada <obenedicta4@gmail.com>
2023-08-17 15:08:39 +0100

    sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- list all shows contained in the hbtn_0d_tvshows database
-SELECT CONCAT(tv_shows.title, '-', tv_show_genres.genre_id) AS record
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER  BY tv_shows.title  ASC, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
