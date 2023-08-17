cf436a81a857c26bd99a10aaf0aeb0d3f340a678
Ada <obenedicta4@gmail.com>
2023-08-17 16:39:38 +0100

    sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,5 +3,5 @@ SELECT tv_genres.name
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE show.title = 'Dexter'
-ORDER BY tv_genre.name ASC;
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
