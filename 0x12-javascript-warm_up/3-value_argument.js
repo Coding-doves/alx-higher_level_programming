dcc71bb684f4a2ac0b8bb0230e4c384e9c0b3d18
Ada <obenedicta4@gmail.com>
2023-09-11 22:59:27 +0100

    js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const nArgs = process.argv[2];
 
-if (nArgs === 0) {
+if (!nArgs) {
   console.log("No argument");
 } else {
   console.log(nArgs);
--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-const nArgs = process.argv[2];
-if (nArgv[2] && nArgv[3]) {
-  console.log(nArgs[2] + ' is ' + nArgs[3]);
-} else {
-  console.log('undefined');
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
