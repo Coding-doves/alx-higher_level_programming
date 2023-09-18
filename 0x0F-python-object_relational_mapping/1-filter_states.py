0a1eda22ec51a7d464500acdcc036e8c4bea1aa5
Ada <106437177+Coding-doves@users.noreply.github.com>
2023-09-18 22:03:19 +0100

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,8 @@ def state_list_name_N(username, password, database_name):
                          db=database_name)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute("""SELECT * FROM states\
+                WHERE name LIKE BINARY 'N%' ORDER BY id ASC""")
 
     states = cur.fetchall()
 
