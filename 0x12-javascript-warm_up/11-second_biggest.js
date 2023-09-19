46c2b4d608a226e9a6f7d0a3a8e2cd06b2865be6
Ada <obenedicta4@gmail.com>
2023-09-19 18:11:03 +0100

    task 7

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const { argv } = require('process');
+const arg  = process.arg.slice(2);
 
-if (argv.length <=3) { console.log(0); }
+if (arg.length < 2) { console.log(0); }
 else {
-  console.log(argv.slice(2).sort((x, y) => x - y)[1]);
+  console.log(arg.map(Number).sort((x, y) => x - y)[1]);
 };
   
