d3b6c3e1e84798f8bdc06a7a5b8c37eae17927a8
Ada <obenedicta4@gmail.com>
2023-09-19 18:49:34 +0100

    update

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
   return function (convertToBase) {
-      return convertToBase.toString(base);
+    return convertToBase.toString(base);
   }
 };
