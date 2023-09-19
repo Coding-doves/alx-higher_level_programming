4ec73ddca51b31d29476c0cd0e0db30a002e4964
Ada <obenedicta4@gmail.com>
2023-09-19 18:25:21 +0100

    update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 const arg  = process.arg.slice(2);
 
-if (arg.length < 2) { console.log(0); }
-else {
+if (arg.length < 2) {
+  console.log(0);
+} else {
   console.log(arg.map(Number).sort((a, b) => b - a)[1]);
 };
   
