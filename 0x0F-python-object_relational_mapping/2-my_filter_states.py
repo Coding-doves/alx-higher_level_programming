fbb50e8963a5283573a1115e18593a3db9ad140d
Ada <obenedicta4@gmail.com>
2023-09-19 14:16:39 +0100

    update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,13 +12,11 @@ def state_list_name_matches(username, password, database_name, state_name_search
 
     cur = db.cursor()
 
-<<<<<<< HEAD
     query = "SELECT * FROM states WHERE name\
           LIKE BINARY '{}'ORDER BY id\
             ASC".format(state_name_searched)
     cur.execute(query, (state_name_searched,))
 
-=======
     cur.execute("SELECT * FROM states \
                 WHERE name LIKE BINARY '{}'\
                 ORDER BY id ASC".format(sys.argv[4])
