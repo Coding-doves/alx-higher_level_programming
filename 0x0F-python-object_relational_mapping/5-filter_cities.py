471a3ac714e861fb485398b18b6b4ee12c2eb582
Ada <obenedicta4@gmail.com>
2023-09-19 23:26:51 +0100

    update

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -2,8 +2,15 @@
 ''' import sql'''
 
 
-def cities_list(username, password, database_name, state_name):
-    '''function'''
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
     db = MySQLdb.connect(host='localhost', port=3306,
                     user=username, passwd=password, db=database_name)
 
@@ -20,16 +27,3 @@ def cities_list(username, password, database_name, state_name):
 
     cursor.close()
     db.close()
-
-
-if __name__ == "__main__":
-    import MySQLdb
-    import sys
-
-
-    if len(sys.argv) == 4:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database_name = sys.argv[3]
-        state_name = sys.argv[4]
-        cities_list(username, password, database_name, state_name)
