ed500d14067fbfd9bf9ad5c5c3283f66637b8e03
Ada <obenedicta4@gmail.com>
2023-09-19 21:49:32 +0100

    update

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -3,9 +3,11 @@
 import MySQLdb
 import sys
 
-def cities_list(username, password, database_name):
-    '''function'''
-<<<<<<< HEAD
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
     db = MySQLdb.connect(
         host='localhost', port=3306, user=username, passwd=password, db=database_name
         )
@@ -13,19 +15,9 @@ def cities_list(username, password, database_name):
     cur = db.cursor()
 
     cur.execute("SELECT cities.id, cities.name,\
-                state.name FROM cities JOIN states ON states.id = cities.state_id\
+                state.name FROM cities JOIN states ON cities.state_id = states.id\
                 ORDER BY cities.id ASC")
-=======
-    db = MySQLdb.connect(host='localhost',
-                         port=3306, user=username,
-                         passwd=password, db=database_name)
-
-    cur = db.cursor()
-
-    cur.execute("SELECT * FROM cities JOIN states ON\
-                cities.states_id = state.id ORDER BY cities.id ASC")
->>>>>>> 589172f9a004b1e229265ebbd4bb877ce034ccc2
-
+    
     cities = cur.fetchall()
 
     for city in cities:
@@ -33,10 +25,3 @@ def cities_list(username, password, database_name):
 
     cur.close()
     db.close()
-
-if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database_name = sys.argv[3]
-        cities_list(username, password, database_name)
