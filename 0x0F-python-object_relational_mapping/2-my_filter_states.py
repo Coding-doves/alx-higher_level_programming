2ddbf3233e76f89c27d4827fbd966dc1bad7a288
Ada <obenedicta4@gmail.com>
2023-09-19 19:24:55 +0100

    update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 ''' import sql'''
-import MySQLdb
-import sys
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
-def state_list_name_matches(username, password, database_name, state_name_searched):
-    '''function'''
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name_searched = sys.argv[4]
+    
     query = "SELECT * FROM states WHERE name\
-          LIKE BINARY '{}' ORDER BY id\
+            LIKE BINARY '{}' ORDER BY id\
             ASC".format(state_name_searched)
     
     db = MySQLdb.connect(
@@ -26,10 +30,5 @@ def state_list_name_matches(username, password, database_name, state_name_search
     cur.close()
     db.close()
 
-if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database_name = sys.argv[3]
-        state_name_searched = sys.argv[4]
+
         state_list_name_matches(username, password, database_name, state_name_searched)
