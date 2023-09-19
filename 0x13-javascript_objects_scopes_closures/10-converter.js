5317952d65fee14b799adc3c3dffcafaf75b1cb8
Ada <obenedicta4@gmail.com>
2023-09-19 18:48:37 +0100

    update

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
   return function (convertToBase) {
-    if (convertToBase < base) {
       return convertToBase.toString(base);
-    } else {
-      return convertToBase.toString(Math.floor(n / base)) + (n % base);
-    }
   }
 };
