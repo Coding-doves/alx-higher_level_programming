9cb264c7ee9e1f2abbccff506eca9a0dfa807f34
Ada <obenedicta4@gmail.com>
2023-09-19 14:36:22 +0100

    update

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,7 @@ const esrever = require('./8-esrever').esrever;
 
 exports.esrever = function (list) {
   let i;
-  let ser = [];
+  const ser = [];
 
   for (i = list.length - 1; i >= 0; i--) {
     ser.push(list[i]);
