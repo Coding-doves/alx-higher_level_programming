5ce4539ce2717c854d6182cc1379f7fcaf925f64
Ada <obenedicta4@gmail.com>
2023-09-19 21:58:10 +0100

    update

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     cursor.execute("SELECT cities.id, cities.name,\
-                state.name FROM cities JOIN states ON cities.state_id = states.id\
+                states.name FROM cities JOIN states ON cities.state_id = states.id\
                 ORDER BY cities.id ASC")
     
     cities = cursor.fetchall()
