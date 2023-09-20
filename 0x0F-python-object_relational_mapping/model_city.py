c1694870928fc056187c9ab069a33aba5eb15d30
Ada <obenedicta4@gmail.com>
2023-09-20 16:34:24 +0100

    update

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,7 +2,6 @@
 '''model city'''
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base
 
 Base = declarative_base()
 
