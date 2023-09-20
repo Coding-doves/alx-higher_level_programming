8a7f0226543cef4554f3e4fa88497b18b1946f13
Ada <obenedicta4@gmail.com>
2023-09-20 15:28:09 +0100

    update

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 '''commenting'''
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
-
 
 if __name__ == "__main__":
     '''commenting'''
@@ -15,7 +15,7 @@ if __name__ == "__main__":
         state_name = sys.argv[4]
 
         engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-            username, password, database_name),pool_pre_ping=True)
+            username, password, database_name), pool_pre_ping=True)
 
         Session = sessionmaker(bind=engine)
         session = Session()
