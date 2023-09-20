fff27066e21c5277675848ff90df9764cf126bce
Ada <obenedicta4@gmail.com>
2023-09-20 15:46:09 +0100

    update

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 '''statement'''
+
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
 from model_state import State
 import sys
 
-
 if __name__ == "__main__":
-    '''statement'''
     username = sys.argv[1]
     password = sys.argv[2]
     database_name = sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
         username, password, database_name), pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
