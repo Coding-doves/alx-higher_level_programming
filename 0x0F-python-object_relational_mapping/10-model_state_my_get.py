ba3c438f014d0daefe2562692cb69a2b69f02fdc
Ada <obenedicta4@gmail.com>
2023-09-20 15:18:22 +0100

    task 7

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -15,12 +15,12 @@ if __name__ == "__main__":
         database_name = sys.argv[3]
         state_name = sys.argv[4]
 
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                               .format(username, password, database_name))
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, database_name),pool_pre_ping=True)
 
         Session = sessionmaker(bind=engine)
         session = Session()
-        state = session.query(State).filter_by(name=state_name).first()
+        state = session.query(State).filter(State.name == state_name).first()
 
         if state:
             print(state.id)
