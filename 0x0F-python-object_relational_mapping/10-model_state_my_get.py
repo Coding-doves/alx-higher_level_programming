06b1b346d59d2ca7e0162877eec782f21c22b93d
Ada <obenedicta4@gmail.com>
2023-09-20 15:36:47 +0100

    update

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -8,22 +8,28 @@ import sys
 
 if __name__ == "__main__":
     '''commenting'''
-    if len(sys.argv) == 5:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database_name = sys.argv[3]
-        state_name = sys.argv[4]
+    if len(sys.argv) != 5:
+        print(
+                "Usage: {} <username> <password> <database> <state>".format(
+                    sys.argv[0])
+        )
+        sys.exit(1)
 
-        engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
-            username, password, database_name), pool_pre_ping=True)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        state = session.query(State).filter(State.name == state_name).first()
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        username, password, database_name), pool_pre_ping=True)
 
-        if state is not None:
-            print(state.id)
-        else:
-            print("Not found")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.name == state_name).first()
 
-        session.close()
+    if state is not None:
+        print(state.id)
+    else:
+        print("Not found")
+
+    session.close()
