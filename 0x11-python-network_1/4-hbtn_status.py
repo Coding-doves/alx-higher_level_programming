8fe9360ad26602baf2049983237a9cb097c5828f
Ada <obenedicta4@gmail.com>
2023-09-29 15:47:24 +0100

    updating networking

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,6 +6,7 @@ import requests
 if __name__ == '__main__':
     url = "https://alx-intranet.hbtn.io/status"
     response = requests.get(url)
+    response.raise_for_status()
 
     print("Body response:")
     print("\t- type:", type(response))
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,3 +7,7 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1]
     pas = requests.get(url)
+
+    data = {'email': email}
+    response = requests.post(url, data=data)
+    print(response.text)
