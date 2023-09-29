e5fba0c7da35839d7264e53ee047cb7a5afe7807
Ada <obenedicta4@gmail.com>
2023-09-29 13:41:07 +0100

    updating networking

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,12 +3,15 @@
 import urllib.request
 
 if __name__ == '__main__':
-    url = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        content = response.read()
-        utf8_content = content.decode('utf-8')
+    try:
+        url = "https://alx-intranet.hbtn.io/status"
+        with urllib.request.urlopen(url) as response:
+            content = response.read()
+            utf8_content = content.decode('utf-8')
 
-        print("Body response:")
-        print('    - type: ', type(content))
-        print('    - content: ', content)
-        print('    - utf8 content: ', utf8_content)
+            print("Body response:")
+            print('    - type: ', type(content))
+            print('    - content: ', content)
+            print('    - utf8 content: ', utf8_content)
+    except Exception as e:
+        pass
