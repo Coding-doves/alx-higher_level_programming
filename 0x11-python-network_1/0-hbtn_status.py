32838d676bfb7bce4ba0de3c6cc631e62297aaa8
Ada <obenedicta4@gmail.com>
2023-09-29 13:18:00 +0100

    networking

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import urllib.request
+
+if __name__ == '__main__':
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        content = response.read()
+        utf8_content = content.decode('utf-8')
+
+        print("Body response:")
+        print('    - type: ', type(content))
+        print('    - content: ', content)
+        print('    - utf8 content: ', utf8_content)
