fe085b80fc313072d429e680b09a4124f37e6c59
Ada <obenedicta4@gmail.com>
2023-09-29 15:36:15 +0100

    updating networking

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    pas = requests.Request(url)
+    pas = requests.get(url)
 
     x_request_id = response.headers.get('X-Request-Id')
     print(x_request_id)
