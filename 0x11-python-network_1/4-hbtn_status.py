0f0bb58e52dfcc18cf673eb716f70b4206176276
Ada <obenedicta4@gmail.com>
2023-09-29 15:55:20 +0100

    updating networking

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,7 +7,6 @@ if __name__ == '__main__':
     url = "https://alx-intranet.hbtn.io/status"
 
     response = requests.get(url)
-    response.raise_for_status()
 
     print("Body response:")
     print("\t- type:", type(response.text))
