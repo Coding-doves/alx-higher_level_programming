9d22f08dc270be6887e7b9c82d78addc46519525
Ada <obenedicta4@gmail.com>
2023-09-29 18:06:53 +0100

    updating networking

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,4 +7,16 @@ import sys
 
 
 if __name__ == '__main__':
-    url = sys.argv[1]
+    username = sys.argv[1]
+    paswd_token = sys.argv[2]
+    #github API
+    url = "https://api.github.com/user"
+
+    #basic authentication
+    auth = (username, passwd_token)
+
+    response = requests.get(url, auth=auth)
+
+    #if response.status_code == 200:
+    data = response.json()
+    print(data)
