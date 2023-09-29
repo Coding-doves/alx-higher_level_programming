44de27a4c3d5d75dc06e33f2da8855e2718a606e
Ada <obenedicta4@gmail.com>
2023-09-29 13:29:55 +0100

    networking

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,14 @@
 import urllib.request
 
 if __name__ == '__main__':
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-        content = response.read()
-        utf8_content = content.decode('utf-8')
+    try:
+        with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+            content = response.read()
+            utf8_content = content.decode('utf-8')
 
-        print("Body response:")
-        print('    - type: ', type(content))
-        print('    - content: ', content)
-        print('    - utf8 content: ', utf8_content)
+            print("Body response:")
+            print('    - type: ', type(content))
+            print('    - content: ', content)
+            print('    - utf8 content: ', utf8_content)
+    except Exception as e:
+        pass
