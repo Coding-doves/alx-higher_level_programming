ee38ec2869e78fb36310c047484bf1e1a3993cc2
Coding-doves <obenedicta4@gmail.com>
2023-10-25 11:51:57 +0100

    web scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,9 +3,8 @@ const request = require('request');
 
 const url = process.argv[2];
 
-request(url, function(error, response, body) {
-  if (error) { console.log(error) }
-  else {
-    console.log('code: ', response && response.statusCode);
+request(url, function (error, response, body) {
+  if (error) { console.log(error); } else {
+    console.log('code:', `${response && response.statusCode}`);
   }
 });
