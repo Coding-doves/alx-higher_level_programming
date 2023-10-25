03114a5d7cd4a6223a6cce80bee0edb45e4e2499
Coding-doves <obenedicta4@gmail.com>
2023-10-25 14:07:23 +0100

    web scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,11 +2,10 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-const apiUrl = 'https://swapi-api.alx-tools.com/api/films/' + movieId
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
 request(apiUrl, function (error, response, body) {
-    if (error) { console.log(error); }
-    else {
-      console.log(JSON.parse(body).title);
-    }
-  });
+  if (error) { console.log(error); } else {
+    console.log(JSON.parse(body).title);
+  }
+});
