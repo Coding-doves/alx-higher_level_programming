d2b7133432011d134ad9726c0e71fb6510d378e7
Coding-doves <obenedicta4@gmail.com>
2023-10-25 15:14:07 +0100

    web scraping

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,9 +1,17 @@
 #!/usr/bin/node
 const fs = require('fs');
+const request = require('request');
 
-const fileP = process.argv[2];
-const string = process.argv[3];
+const url = process.argv[2];
+const fileP = process.argv[3];
 
-fs.writeFile(fileP, string, { encoding: 'utf-8' }, (err) => {
-  if (err) { console.log(err); }
+request(url, 'utf-8', function (error, response, body) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  fs.writeFile(fileP, body, (err) => {
+    if (err) { console.log(err); }
+  });
 });
