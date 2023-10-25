f0148485a627ec7f6f92b2109052f1f49a6f3e98
Coding-doves <obenedicta4@gmail.com>
2023-10-25 11:13:43 +0100

    web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 fileP = process.argv[2];
 
-fs.readfile(fileP, (err, data) => {
+fs.readFile(fileP, 'utf-8', (err, data) => {
   if (err) { console.log(err); }
-  else { console.log(err); }
+  else { console.log(data); }
 })
